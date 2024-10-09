b5d6a0b535d9cfc442605f043e7452c94e4c07ac
Nobert-Muma <mumanobert17@gmail.com>
2024-10-10 00:10:23 +0300

    Chatterbox Lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,42 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method=='GET':
+        response_body=[message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+        return make_response(messages, 200)
 
-@app.route('/messages/<int:id>')
+    elif request.method=='POST':
+        data=request.json
+        new_message=Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        return jsonify(new_message.to_dict()), 201
+   
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message=Message.query.filter_by(id=id).first()
+    if request.method=='PATCH':
+        data=request.json
+        for attr, value in data.items():
+            setattr(message, attr, value)
+        db.session.commit()
+        response_body=message.to_dict()
+        response_status=200
+    elif request.method=='DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        response_body={
+            "delete_successful":True,
+            "message":"Message deleted."
+        }
+        response_status=200
+    response=make_response(jsonify(response_body), response_status)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555)
